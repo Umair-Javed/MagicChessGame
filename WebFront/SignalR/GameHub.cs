3fc27224e5f62b1cf96e2d6a702b13f999e2119f
umair-servicePath <umair.javed@servicepath.co>
2023-11-25 18:46:42 +0500

    signalR

--- WebFront/SignalR/GameHub.cs
+++ WebFront/SignalR/GameHub.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Cryptography.Xml;
+using WebFront.Services;
 
 public class GameHub : Hub
 {
+    private readonly IMongoDBService _mongoDBService;
+
+    public GameHub(IMongoDBService mongoDBService)
+    {
+        _mongoDBService = mongoDBService;
+    }
     public async Task FlipCoin()
     {
         // Broadcast the coin flip to all connected clients
@@ -10,19 +18,20 @@ public class GameHub : Hub
         // Broadcast the coin flip to all clients in the "gameGroup"
         //Groupname should be combination of user1,user2,token
         //Fetch tableId
-        
-        await Clients.Group("HU").SendAsync("CoinFlipped");
+        //Fetch tableHTML
+        var tableHTML = _mongoDBService.GetSessionById("655f9f566d63483e20a2b7ed");
+        await Clients.Group("HU").SendAsync("CoinFlipped", tableHTML.ChessBoardHtml);
     }
 
-    public async Task AddToGameGroup(string userId)
+    public async Task AddToGameGroup(string tableId)
     {
-        var tableId = "HU"; //GetTableId
+        tableId = "HU"; //GetTableId
         await Groups.AddToGroupAsync(Context.ConnectionId, tableId);
     }
 
-    public async Task RemoveFromGameGroup(string userId)
+    public async Task RemoveFromGameGroup(string tableId)
     {
-        var tableId = "HU";
+        tableId = "HU";
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, tableId);
     }
 }
